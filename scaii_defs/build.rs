4c2694a35c8160a4d129564f1c046abd380b8af4
Zoe Juozapaitis <Jragonmiris@gmail.com>
2017-10-19 03:02:53 -0700

    Refactor protobuf into multiple files

--- scaii_defs/build.rs
+++ scaii_defs/build.rs
@@ -1,18 +1,15 @@
-extern crate prost_build;
 extern crate glob;
+extern crate prost_build;
 
 fn main() {
     use glob::glob;
 
-    prost_build::compile_protos(
-        &["../common_protos/universal_messages.proto"],
-        &["../common_protos"],
-    ).unwrap();
+    prost_build::compile_protos(&["../common_protos/scaii.proto"], &["../common_protos"]).unwrap();
 
     // Make our protobuf file a reason to recompile
     // (See http://doc.crates.io/build-script.html for more info
     // on outputs)
-    println!("cargo:rerun-if-changed=../common_protos/universal_messages.proto");
+    println!("cargo:rerun-if-changed=../common_protos/scaii.proto");
 
     // Unfortunately, rerun-if-changed overrides Cargo's default handling, which
     // is to rerun if any of our files change, so we have to re-add all our source
