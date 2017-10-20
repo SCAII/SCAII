27689a92b367204f5d5a25c6be157a80e8b061da
Zoe Juozapaitis <Jragonmiris@gmail.com>
2017-10-19 19:46:56 -0700

    Fix up linux scripts

--- viz/gen_protos.sh
+++ viz/gen_protos.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)/js"
-cd DIR
+cd $DIR
 
 printf "Building protobuf file\n"
-protoc --proto_path="$DIR/../../common_protos" --js_out=library=vizProto,binary:"." "$DIR/../../common_protos/*.proto"
+
+
+PROTO_INCLUDE=`realpath "$DIR/../../common_protos"`
+PROTOS="$PROTO_INCLUDE"/*.proto
+
+protoc --proto_path="$PROTO_INCLUDE" --js_out=library=vizProto,binary:"." $PROTOS
--- viz/setup_deps.sh
+++ viz/setup_deps.sh
@@ -5,14 +5,14 @@ cd $DIR
 
 source "../gen_protos.sh"
 
-if [ -d "closure-library" ]; then
+if [ ! -d "closure-library" ]; then
     printf "\nCloning google closure library dependency\n"
     git clone https://github.com/google/closure-library
 else
     printf "\nGoogle closure library exists ... skipping\n"
 fi
 
-if [ -d "protobuf_js" ]; then
+if [ ! -d "protobuf_js" ]; then
     printf "\nCloning and setting up protobuf-js dependency\n"
     git clone https://github.com/google/protobuf
     mv protobuf/js ./protobuf_js
