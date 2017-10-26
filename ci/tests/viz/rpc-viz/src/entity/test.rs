2761a1aa840780f1bbd218bcc4e939a650e98282
Jed Irvine <jedirv@gmail.com>
2017-10-25 21:27:52 -0700

    in test mode, state is echoed back from viz.  In non-test mode, UserCommand::UserCommandType::NONE is returned

--- ci/tests/viz/rpc-viz/src/entity/test.rs
+++ ci/tests/viz/rpc-viz/src/entity/test.rs
@@ -1,6 +1,9 @@
 use super::*;
 use FUZZY_EQ_THRESH;
 
+use websocket::client::sync::Client;
+use websocket::stream::sync::TcpStream;
+use scaii_defs::protos::ScaiiPacket;
 #[test]
 fn test_to_from_proto() {
     use rand;
@@ -9,13 +12,11 @@ fn test_to_from_proto() {
     let proto = entity.to_proto();
 
     match IdEntity::from_proto(&proto) {
-        Err(err) => {
-            panic!(
-                "Could not decode proto just created: {}. Proto: {:?}",
-                err,
-                proto
-            )
-        }
+        Err(err) => panic!(
+            "Could not decode proto just created: {}. Proto: {:?}",
+            err,
+            proto
+        ),
         Ok(got) => assert!(entity.fuzzy_eq(&got, FUZZY_EQ_THRESH)),
     }
 }
