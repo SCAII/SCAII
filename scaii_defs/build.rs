c375c6dafe6943cbac0e638d36788b68ced2ffa3
Zoe Juozapaitis <Jragonmiris@gmail.com>
2017-11-03 17:48:34 -0700

    Add new protos to build script

--- scaii_defs/build.rs
+++ scaii_defs/build.rs
@@ -9,7 +9,9 @@ fn main() {
     // Make our protobuf file a reason to recompile
     // (See http://doc.crates.io/build-script.html for more info
     // on outputs)
-    println!("cargo:rerun-if-changed=../common_protos/scaii.proto");
+    for entry in glob("../common_protos/*.rs") {
+        println!("cargo:rerun-if-changed={}", path.display());
+    }
 
     // Unfortunately, rerun-if-changed overrides Cargo's default handling, which
     // is to rerun if any of our files change, so we have to re-add all our source
