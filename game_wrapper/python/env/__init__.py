7febaa3dc26fb7d587a37e0cb2204b097a21aa5a
Zoe Juozapaitis <jragonmiris@gmail.com>
2018-02-15 16:18:15 -0800

    First attempt

--- game_wrapper/python/env/__init__.py
+++ game_wrapper/python/env/__init__.py
@@ -14,20 +14,19 @@ from .state import SkyState
 class SkyRtsEnv(ScaiiEnv):
 
     def __init__(self, mock_core=None, state_type=SkyState, action_type=MoveList):
-        import os
         import platform
 
         super().__init__(mock_core=mock_core, state_type=state_type, action_type=action_type)
 
         if platform.system().lower() == 'windows':
             super().load_backend(
-                os.environ['HOME'] + "/.scaii/backends/bin/sky-rts.dll")
+                "backends/bin/sky-rts.dll")
         elif platform.system().lower() == 'darwin':
             super().load_backend(
-                os.environ['HOME'] + "/.scaii/backends/bin/sky-rts.dylib")
+                "backends/bin/sky-rts.dylib")
         elif platform.system().lower() == 'linux':
             super().load_backend(
-                os.environ['HOME'] + "/.scaii/backends/bin/libsky-rts.so")
+                "backends/bin/libsky-rts.so")
 
     def load_scenario(self, path):
         from ..protos.sky_rts_pb2 import Config
