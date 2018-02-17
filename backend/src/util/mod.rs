5cfb081da6371d74a856b61b45645697fa0c3228
Zoe Juozapaitis <Jragonmiris@gmail.com>
2018-02-17 00:25:05 -0800

    Upgrade to using patch and fix resulting bugs

--- backend/src/util/mod.rs
+++ backend/src/util/mod.rs
@@ -1,30 +1,18 @@
 //! Utility functions for RTS-wide use
 
-/// The seed size of the RNG
-const SEED_SIZE: usize = 256;
-
-use rand::{Isaac64Rng, Rng, SeedableRng};
+use rand::{Isaac64Rng, SeedableRng};
 use rand;
 
 use scaii_defs::protos::MultiMessage;
 
 /// Create a new seeded RNG.
 pub fn make_rng() -> Isaac64Rng {
-    Isaac64Rng::from_seed(&seed()[..])
+    Isaac64Rng::from_rng(&mut rand::thread_rng()).expect("Could not make RNG")
 }
 
 /// Reseed the RNG so the state diverges.
 pub fn diverge(rng: &mut Isaac64Rng) {
-    rng.reseed(&seed()[..]);
-}
-
-/// Randomly create a new seed using the thread local RNG.
-fn seed() -> [u64; SEED_SIZE] {
-    let mut buf = [0; SEED_SIZE];
-    for i in 0..SEED_SIZE {
-        buf[i] = rand::thread_rng().gen();
-    }
-    buf
+    *rng = Isaac64Rng::from_rng(&mut rand::thread_rng()).expect("Could not reseed RNG");
 }
 
 /// Create an acknowledgement message
