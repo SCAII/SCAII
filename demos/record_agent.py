128ceeffae684b7d5e11862586c9d81fd36fb734
Zoe Juozapaitis <jragonmiris@gmail.com>
2018-03-20 16:50:18 -0700

    Provisional demo update for chart

--- demos/record_agent.py
+++ demos/record_agent.py
@@ -1,5 +1,5 @@
 from scaii.env.sky_rts.env.scenarios.tower_example import TowerExample
-from scaii.env.explanation import Explanation
+from scaii.env.explanation import Explanation, BarChart, BarGroup, Bar
 import numpy as np
 
 env = TowerExample()
@@ -11,20 +11,43 @@ s = env.reset(record=True)
 
 print("acting")
 act = env.new_action()
-act.attack_quadrant(2)
 
-explanation = Explanation("Fake Random Saliency Info", (40,40))
-layers = np.random.random((40,40,6))
+explanation = Explanation("Fake Random Saliency Info", layer_shape=(40, 40))
+chart = BarChart("Move Explanation", "Actions", "QVal By Reward Type")
 layer_names = ["HP", "Type 1", "Type 2", "Type 3", "Friend", "Enemy"]
 
+max_quad = 0
+max_value = -np.inf
+for quad in range(1, 5):
+    layers = np.random.random((40, 40, 6))
+    key = "BarGroup{}".format(quad)
+    group = BarGroup("Attack {}".format(env.actions()[quad]), key=key)
+    explanation.add_layers(layer_names, layers, key=key)
 
-explanation.with_layers(layer_names, layers)
-print(len(explanation._proto.layers))
+    value = 0.0
+    for r_type in env.reward_types():
+        b_layers = np.random.random(40, 40, 6)
+        key = "BarGroup{}Bar{}".format(quad, r_type)
 
+        bar_val = np.random.rand()
+        value += bar_val
+        bar = Bar(r_type, bar_val, key=key)
+
+        group.add_bar(bar)
+        explanation.add_layers(layer_names, b_layers, key=key)
+
+    chart.add_bar_group(group)
+
+    if value > max_value:
+        max_quad = quad
+        max_value = value
+
+act.attack_quadrant(max_quad)
 s = env.act(act, explanation=explanation)
 
 if not s.is_terminal():
-    raise Exception("Should not get in loop")
+    raise Exception(
+        "State in TowerExample should always be terminal after action")
 
 print("Reward is:", s.reward, "Terminal?:", s.is_terminal())
 print("With types:", s.typed_reward)
