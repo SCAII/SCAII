6cf687c2a051da03cb513fe7a49bfcbd4cadff15
Zoe Juozapaitis <jragonmiris@gmail.com>
2018-03-20 17:55:32 -0700

    Add bar chart to record demo

--- demos/record_agent.py
+++ demos/record_agent.py
@@ -2,10 +2,18 @@ from scaii.env.sky_rts.env.scenarios.tower_example import TowerExample
 from scaii.env.explanation import Explanation, BarChart, BarGroup, Bar
 import numpy as np
 
+
+def invert_actions(env):
+    out = dict([])
+    for (k, v) in env.actions()['actions'].items():
+        out[v] = k
+
+
 env = TowerExample()
 print("Possible reward types:", env.reward_types())
 print("Possible actions:", env.actions())
 print("Action description", env.action_desc())
+actions = invert_actions(env)
 
 s = env.reset(record=True)
 
@@ -21,17 +29,17 @@ max_value = -np.inf
 for quad in range(1, 5):
     layers = np.random.random((40, 40, 6))
     key = "BarGroup{}".format(quad)
-    group = BarGroup("Attack {}".format(env.actions()[quad]), key=key)
+    group = BarGroup("Attack {}".format(quad), saliency_key=key)
     explanation.add_layers(layer_names, layers, key=key)
 
     value = 0.0
     for r_type in env.reward_types():
-        b_layers = np.random.random(40, 40, 6)
+        b_layers = np.random.random((40, 40, 6))
         key = "BarGroup{}Bar{}".format(quad, r_type)
 
         bar_val = np.random.rand()
         value += bar_val
-        bar = Bar(r_type, bar_val, key=key)
+        bar = Bar(r_type, bar_val, saliency_key=key)
 
         group.add_bar(bar)
         explanation.add_layers(layer_names, b_layers, key=key)
@@ -41,6 +49,8 @@ for quad in range(1, 5):
     if value > max_value:
         max_quad = quad
         max_value = value
+
+explanation.with_bar_chart(chart)
 
 act.attack_quadrant(max_quad)
 s = env.act(act, explanation=explanation)
