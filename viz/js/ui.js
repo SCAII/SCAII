64ec02a2c0c415d17035cd0540b106841613a9a8
Jed Irvine <jedirv@gmail.com>
2018-05-01 13:59:18 -0700

    layout fixes

--- viz/js/ui.js
+++ viz/js/ui.js
@@ -89,7 +89,7 @@ function configureRewindButton(){
 	rewindButton.onclick = tryRewind;
 	$("#rewind-control").append(rewindButton);
 	$("#rewindButton").css("padding-top","4px");
-	$("#rewindButton").css("margin-left","100px");
+	$("#rewindButton").css("margin-left","150px");
 	$("#rewindButton").css("opacity", "0.6");
 	rewindButton.disabled = true;
 }
