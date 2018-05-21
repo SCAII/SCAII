5359dd05621abfefd882e3fe3a18c7a08b35caeb
Zoe Juozapaitis <Jragonmiris@gmail.com>
2018-05-21 01:28:05 -0700

    Add data store

--- backends/sky-rts/src/engine/systems/lua/userdata.rs
+++ backends/sky-rts/src/engine/systems/lua/userdata.rs
@@ -7,6 +7,110 @@ use std::collections::HashMap;
 
 use rand::Rng;
 
+/// This is a workaround for Lua not having any serializable elements,
+/// and no clear semantics to serialize a `Table` object. This may be reworked
+/// later with a better API based around a custom foreign serialization implementation
+/// for Table and other Lua types.
+#[derive(Serialize, Deserialize, Clone, PartialEq, Debug, Default)]
+pub struct DataStore {
+    int_data: HashMap<String, i64>,
+    float_data: HashMap<String, f64>,
+    string_data: HashMap<String, String>,
+    bool_data: HashMap<String, bool>,
+}
+
+impl DataStore {
+    fn remove_key(&mut self, idx: &str) {
+        self.int_data.remove(idx);
+        self.float_data.remove(idx);
+        self.string_data.remove(idx);
+        self.bool_data.remove(idx);
+    }
+
+    pub fn len(&self) -> usize {
+        self.int_data.len() + self.float_data.len() + self.string_data.len() + self.bool_data.len()
+    }
+
+    pub fn display_all(&self, out: Option<HashMap<String, String>>) -> HashMap<String, String> {
+        let mut out = out.unwrap_or_else(|| HashMap::with_capacity(self.len()));
+        out.clear();
+
+        let iter = self.int_data
+            .iter()
+            .map(|(k, v)| (k.clone(), format!("{}", v)));
+        out.extend(iter);
+
+        let iter = self.float_data
+            .iter()
+            .map(|(k, v)| (k.clone(), format!("{}", v)));
+        out.extend(iter);
+
+        let iter = self.bool_data
+            .iter()
+            .map(|(k, v)| (k.clone(), format!("{}", v)));
+        out.extend(iter);
+
+        // Iter.cloned doesn't play nice with hash map's iter method for some reason
+        out.extend(self.string_data.iter().map(|(k, v)| (k.clone(), v.clone())));
+
+        out
+    }
+}
+
+impl UserData for DataStore {
+    fn add_methods(methods: &mut UserDataMethods<Self>) {
+        use rlua::{MetaMethod, Value};
+        use rlua::String as LuaString;
+
+        methods.add_meta_method(MetaMethod::Index, |lua, this, idx: LuaString| {
+            let idx = idx.to_str()?;
+
+            if let Some(val) = this.int_data.get(idx) {
+                Ok(Value::Integer(*val))
+            } else if let Some(val) = this.float_data.get(idx) {
+                Ok(Value::Number(*val))
+            } else if let Some(val) = this.string_data.get(idx) {
+                Ok(Value::String(lua.create_string(val)?))
+            } else if let Some(val) = this.bool_data.get(idx) {
+                Ok(Value::Boolean(*val))
+            } else {
+                panic!("Lua used unexpected index {:?}", idx);
+            }
+        });
+
+        methods.add_meta_method_mut(
+            MetaMethod::NewIndex,
+            |_, this, (idx, val): (LuaString, Value)| {
+                let idx = idx.to_str()?;
+
+                this.remove_key(idx);
+
+                match val {
+                    Value::Integer(val) => {
+                        this.int_data.insert(idx.to_string(), val);
+                    }
+                    Value::Number(val) => {
+                        this.float_data.insert(idx.to_string(), val);
+                    }
+                    Value::Boolean(val) => {
+                        this.bool_data.insert(idx.to_string(), val);
+                    }
+                    Value::String(val) => {
+                        this.string_data
+                            .insert(idx.to_string(), val.to_str()?.to_string());
+                    }
+                    Value::Nil => {
+                        // ... do nothing, we interpret writing nil as deleting a key
+                    }
+                    _ => panic!("Unsupported type"), // todo, change when we upgrade to failure
+                };
+
+                Ok(())
+            },
+        );
+    }
+}
+
 #[derive(Copy, Clone)]
 pub struct UserDataRng<R: Rng + 'static> {
     pub rng: *mut R,
@@ -112,5 +216,77 @@ impl UserData for UserDataUnit {
         methods.add_method("faction", |_, this, ()| Ok(this.faction.0));
         methods.add_method("unit_type", |_, this, ()| Ok(this.u_type.clone()));
         methods.add_method("hp", |_, this, ()| Ok(this.hp.curr_hp));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rlua::Lua;
+
+    const SETUP_PROGRAM: &'static str = r#"store.x = 5
+store.a_bool = true
+store.str = "hello"
+store.pi = 3.14"#;
+
+    fn setup_data_store() -> Lua {
+        use super::DataStore;
+
+        let lua = Lua::new();
+        lua.globals().set("store", DataStore::default()).unwrap();
+
+        lua.exec::<()>(SETUP_PROGRAM, Some("Data Store")).unwrap();
+
+        lua
+    }
+
+    #[test]
+    fn lua_data_store_add() {
+        use super::DataStore;
+
+        let lua = setup_data_store();
+        let store: DataStore = lua.globals().get("store").unwrap();
+
+        assert_eq!(store.int_data.get("x"), Some(&5));
+        assert_eq!(store.float_data.get("pi"), Some(&3.14));
+        assert_eq!(store.bool_data.get("a_bool"), Some(&true));
+        assert_eq!(store.string_data.get("str").map(|s| &**s), Some("hello"));
+    }
+
+    #[test]
+    fn lua_data_store_remove() {
+        use super::DataStore;
+
+        let lua = setup_data_store();
+
+        lua.exec::<()>("store.pi = nil", Some("remove")).unwrap();
+
+        let store: DataStore = lua.globals().get("store").unwrap();
+
+        assert!(store.float_data.get("pi").is_none());
+    }
+
+    #[test]
+    fn lua_data_store_replace() {
+        use super::DataStore;
+
+        let lua = setup_data_store();
+
+        lua.exec::<()>("store.pi = 12", Some("replace")).unwrap();
+
+        let store: DataStore = lua.globals().get("store").unwrap();
+
+        assert!(store.float_data.get("pi").is_none());
+        assert_eq!(store.int_data.get("pi"), Some(&12));
+    }
+
+    #[test]
+    fn lua_data_store_read() {
+        use rlua::Number;
+
+        let lua = setup_data_store();
+
+        let pi = lua.exec::<Number>("return store.pi", Some("read")).unwrap();
+
+        assert_eq!(pi, 3.14);
     }
 }
