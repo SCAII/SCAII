ca525075753e2e5f389405628f097c71b5327796
Zoe Juozapaitis <Jragonmiris@gmail.com>
2018-06-10 18:10:42 -0700

    Fix reward keyframe bug

--- backends/sky-rts/src/engine/systems/lua/mod.rs
+++ backends/sky-rts/src/engine/systems/lua/mod.rs
@@ -15,6 +15,8 @@ use engine::resources::{
 };
 use rand::Isaac64Rng;
 
+use std::collections::HashMap;
+
 pub(crate) mod userdata;
 
 #[derive(SystemData)]
@@ -44,6 +46,7 @@ pub struct LuaSystemData<'a> {
 
 pub struct LuaSystem {
     lua: Lua,
+    immediate_reward: HashMap<String, f64>,
 }
 
 unsafe impl Send for LuaSystem {}
@@ -53,6 +56,8 @@ impl<'a> System<'a> for LuaSystem {
 
     fn run(&mut self, mut sys_data: Self::SystemData) {
         use self::userdata::{UserDataReadWorld, UserDataRng, UserDataUnit, UserDataWorld};
+
+        self.immediate_reward.clear();
 
         let world = UserDataWorld::new(UserDataRng {
             rng: &mut *sys_data.rng,
@@ -95,15 +100,13 @@ impl<'a> System<'a> for LuaSystem {
             let dead_type = sys_data.unit_type.tag_map.get(&unit1.u_type).unwrap();
 
             if faction.0 == 0 && dead_type.death_penalty != 0.0 {
-                *sys_data
-                    .reward
-                    .0
+                *self
+                    .immediate_reward
                     .entry(dead_type.death_type.clone())
                     .or_insert(0.0) += dead_type.death_penalty;
             } else if faction.0 == 1 && killer_faction.0 == 0 && dead_type.kill_reward != 0.0 {
-                *sys_data
-                    .reward
-                    .0
+                *self
+                    .immediate_reward
                     .entry(dead_type.kill_type.clone())
                     .or_insert(0.0) += dead_type.kill_reward;
             }
@@ -141,17 +144,15 @@ impl<'a> System<'a> for LuaSystem {
                     .filter_map(|d| if (d.1).0 != 0 { Some(d.0) } else { None })
                     .sum();
 
-                *sys_data
-                    .reward
-                    .0
+                *self
+                    .immediate_reward
                     .entry(u_type.dmg_deal_type.clone())
                     .or_insert(0.0) += enemy_dmg
             } else if u_type.damage_deal_reward.unwrap() != 0.0 {
                 let enemy_dmg_ct = dmg_dealt.by_source.iter().filter(|d| (d.1).0 != 0).count();
 
-                *sys_data
-                    .reward
-                    .0
+                *self
+                    .immediate_reward
                     .entry(u_type.dmg_deal_type.clone())
                     .or_insert(0.0) += (enemy_dmg_ct as f64) * u_type.damage_deal_reward.unwrap();
             }
@@ -164,15 +165,13 @@ impl<'a> System<'a> for LuaSystem {
             let u_type = sys_data.unit_type.tag_map.get(&tag.0).unwrap();
 
             if u_type.damage_recv_penalty.is_none() {
-                *sys_data
-                    .reward
-                    .0
+                *self
+                    .immediate_reward
                     .entry(u_type.dmg_recv_type.clone())
                     .or_insert(0.0) += hp_change.0
             } else if u_type.damage_recv_penalty.unwrap() != 0.0 {
-                *sys_data
-                    .reward
-                    .0
+                *self
+                    .immediate_reward
                     .entry(u_type.dmg_recv_type.clone())
                     .or_insert(0.0) += u_type.damage_recv_penalty.unwrap();
             }
@@ -195,7 +194,7 @@ impl<'a> System<'a> for LuaSystem {
         let r_types = &sys_data.r_types.0;
         for (r_type, reward) in world.rewards {
             assert!(r_types.contains(&r_type));
-            *sys_data.reward.0.entry(r_type).or_insert(0.0) += reward;
+            *self.immediate_reward.entry(r_type).or_insert(0.0) += reward;
         }
 
         sys_data.spawn_buf.0.extend(world.spawn.drain(..));
@@ -227,7 +226,8 @@ impl<'a> System<'a> for LuaSystem {
             }
         }
 
-        for (r_type, reward) in sys_data.reward.0.iter() {
+        for (r_type, reward) in &self.immediate_reward {
+            *sys_data.reward.0.entry(r_type.clone()).or_insert(0.0) += reward;
             *sys_data
                 .cum_reward
                 .0
@@ -239,13 +239,19 @@ impl<'a> System<'a> for LuaSystem {
 impl LuaSystem {
     /// Creates a new `LuaSystem` with a new context.
     pub fn new() -> Self {
-        LuaSystem { lua: Lua::new() }
+        LuaSystem {
+            lua: Lua::new(),
+            immediate_reward: HashMap::default(),
+        }
     }
 
     /// Creates a new Lua system from the given context
     #[allow(dead_code)]
     pub fn from_lua(lua: Lua) -> Self {
-        LuaSystem { lua: lua }
+        LuaSystem {
+            lua: lua,
+            immediate_reward: HashMap::default(),
+        }
     }
 
     /// Executes a Lua script in the current Lua context, loading any
