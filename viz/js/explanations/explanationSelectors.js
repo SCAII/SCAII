333b924a9b1982d7f747c4d7496c7f50056301eb
Jed Irvine <jedirv@gmail.com>
2018-08-10 17:13:53 -0700

    rename uiIndex to decisionPointNumber for clarity

--- viz/js/explanations/explanationSelectors.js
+++ viz/js/explanations/explanationSelectors.js
@@ -11,13 +11,13 @@ function renderExplanationSelectors() {
 	explanationBoxMap = {};
 	while (index < expl_count){
 		var step = explanation_steps[index];
-		var uiIndex =index + 1;
-		configureExplanationSelectorDiamond(uiIndex, step);
+		var decisionPointNumber =index + 1;
+		configureExplanationSelectorDiamond(decisionPointNumber, step);
 		index = index + 1;
 	}
 }
 
-function configureExplanationSelectorDiamond(uiIndex,step){
+function configureExplanationSelectorDiamond(decisionPointNumber,step){
 	var x = getXOriginOfDecisionPointAtStep(step);
 	var y = explanationControlYPosition;
 	var halfWidth;
@@ -26,8 +26,8 @@ function configureExplanationSelectorDiamond(uiIndex,step){
 	var currentStep = sessionIndexManager.getCurrentIndex();
 	var ctx = expl_ctrl_ctx;
 	if (currentStep == step) {
-		showingDecisionNumber = uiIndex;
-        $("#winning-action-label").html("Chosen move at D" + uiIndex + ": " + winningActionForStep[step]);
+		showingDecisionNumber = decisionPointNumber;
+        $("#winning-action-label").html("Chosen move at D" + decisionPointNumber + ": " + winningActionForStep[step]);
 		ctx.font = "16px Arial bold";
 		halfWidth = explanationPointBigDiamondHalfWidth;
         halfHeight = explanationPointBigDiamondHalfWidth;
@@ -103,7 +103,7 @@ function configureExplanationSelectorDiamond(uiIndex,step){
 	}
 	ctx.font = "Arial";
 	var textCenterY = explanationControlYPosition + 5;
-	ctx.fillText('D' + uiIndex,textCenterX,textCenterY);
+	ctx.fillText('D' + decisionPointNumber,textCenterX,textCenterY);
 
 	//ctx.rect(upper_left_x, upper_left_y, rect_width, rect_height);
 	var eBox = getExplanationBox(leftVertexX, rightVertexX, topVertexY, bottomVertexY, step);
