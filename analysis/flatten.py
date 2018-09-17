463ccf16f8f78162a21065614f6fdbde50ca0964
Jed Irvine <jedirv@gmail.com>
2018-09-17 15:09:40 -0700

    need default value for key

--- analysis/flatten.py
+++ analysis/flatten.py
@@ -60,6 +60,7 @@ def replace_all_delimeters_with_commas(line):
 def get_key_for_line(line):
     key = "UNKNOWN"
     fields = line.split(',')
+    print("{}".format(line))
     if ("userClick" in line):
         key = get_key_for_user_click_line(line)
     elif ("startMouseOverSaliencyMap" in line):
@@ -76,6 +77,7 @@ def get_key_for_line(line):
     return key
 
 def get_key_for_user_click_line(line):
+    key = "UNKNOWN"
     if ("answerQuestion" in line):
         #need to look into the saved off click
         if ("(NA)" in line):
